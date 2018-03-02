16f9aeb111fab074070408159c2c1f772cf8f86e
migueltanada <tanada.miguel@gmail.com>
2018-03-02 19:47:31 +0800

    Update basicTitleTest.java

--- src/test/java/basicTitleTest.java
+++ src/test/java/basicTitleTest.java
@@ -29,10 +29,10 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 public class basicTitleTest {
 	
 	//URL to visit
-	public String QWE="http://tomcat.13.115.55.128.xip.io/CurrencyConverter-1/";
+	public String QWE="https://www.facebook.com/";
 	
 	//Title to check
-	public String QWE1="Currency Converter";
+	public String QWE1="Facebook";
 	public String nodeURL = "http://selenium-hub:4444/wd/hub";
 
 	public static void main(String[] args){
